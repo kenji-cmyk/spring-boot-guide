b4467dae91f5f1c51fe0aecad38f0b049e0e40ff
AnhKhoi <khoina231@gmail.com>
2026-05-14 08:39:19 +0700

    soap-spring

---
+++ soap-spring/src/main/java/com/kna/soap/SoapSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kna.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapSpringApplication.class, args);
+    }
+
+}
